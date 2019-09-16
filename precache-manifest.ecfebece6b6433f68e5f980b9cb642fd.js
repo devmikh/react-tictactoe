7c10ad6305ba5981979e8a99c5bd594b5a294847
Mikhail Serebriakov <mishacyb@gmail.com>
2019-09-16 14:07:46 -0700

    Updates

---
+++ precache-manifest.ecfebece6b6433f68e5f980b9cb642fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b63877f75a7f9efae6c006f8fb8541",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "3dc5a6abe3c5b12e851c",
+    "url": "/react-tictactoe/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "7da300adce1e81d9c6f3",
+    "url": "/react-tictactoe/static/js/2.31654b1e.chunk.js"
+  },
+  {
+    "revision": "3dc5a6abe3c5b12e851c",
+    "url": "/react-tictactoe/static/js/main.bd22090c.chunk.js"
+  },
+  {
+    "revision": "28a1ef29365ade12f92c",
+    "url": "/react-tictactoe/static/js/runtime~main.5fe4afdf.js"
+  }
+]);
